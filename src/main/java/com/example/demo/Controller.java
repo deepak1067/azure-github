571671465e3183e8c0fb94cb7e56a3f32e3f3674
deepak1067 <deepak@knoldus.com>
2023-07-09 12:39:19 +0530

    first commit

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller
+{
+    @GetMapping("/path")
+    public String hello()
+    {
+        return "27836t82734626";
+    }
+    
+}
